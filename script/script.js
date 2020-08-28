beb0e058af6b9c38f875676498c0f00a9058af28
marco <marcodecrema@libero.it>
2020-08-28 16:14:14 +0200

    prova5

--- script/script.js
+++ script/script.js
@@ -1,17 +1,18 @@
+// Variabile che richiama la funzione random
 var valore = generatoreNumero();
+// Funzione per generare numero random
 function generatoreNumero(numeri) {
   var numeri = Math.floor(Math.random()*100)+1;
   return numeri;
 };
-
+// Funzione per ciclare i numeri random
 function cicloNumRandom(valore) {
   var min = 1;
   var max = 16;
   var insiemeNumeri= "";
   var listaNumeri = [];
   for (i=min; i<= max; i++){
-    insiemeNumeri = valore * i;
-    listaNumeri.push(insiemeNumeri);
+    listaNumeri.push(valore);
   }
   return listaNumeri;
 };
