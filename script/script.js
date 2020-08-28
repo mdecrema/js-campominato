6bf4a23c842895f8a3ae198567e9eea116d568e9
marco <marcodecrema@libero.it>
2020-08-28 16:10:55 +0200

    prova4

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-var ciao = generatoreNumero();
+var valore = generatoreNumero();
 function generatoreNumero(numeri) {
   var numeri = Math.floor(Math.random()*100)+1;
   return numeri;
 };
 
-function cicloNumRandom(ciao) {
+function cicloNumRandom(valore) {
   var min = 1;
   var max = 16;
   var insiemeNumeri= "";
   var listaNumeri = [];
   for (i=min; i<= max; i++){
-    bu = ciao * i;
+    insiemeNumeri = valore * i;
     listaNumeri.push(insiemeNumeri);
   }
   return listaNumeri;
