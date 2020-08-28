21c51d2a1030c41955a20c38fb25a5fb6d7d249f
marco <marcodecrema@libero.it>
2020-08-28 17:40:53 +0200

    prova8

--- script/script.js
+++ script/script.js
@@ -5,14 +5,16 @@ var check = console.log(generatoreNum());
 function generatoreNum() {
   var insiemeNumeri = [];
   for (i=1; i<=16; i++) {
-    var numGen = Math.floor(Math.random()*cento.length);
+    var numGen = Math.floor(Math.random()* cento.length);
+    cento.splice(numGen, numGen[i]);
     insiemeNumeri.push(numGen);
   }
   return insiemeNumeri;
 };
 
+
 function numeriPossibili() {
-  insiemeNumPoss = [];
+  var insiemeNumPoss = [];
   for (i=1; i<=100; i++) {
     insiemeNumPoss.push(i);
   }
